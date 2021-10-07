d3f3ddce2424c02841474aedd95098b4fa9136a3
iamstan13y <iamstan13y@gmail.com>
2021-10-07 03:51:58 +0200

    Downgrade to .NET 5.0

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace PocketBook.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public virtual DbSet<User> Users { get; set; }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
         {
