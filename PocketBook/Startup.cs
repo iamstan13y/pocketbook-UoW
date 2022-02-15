c47c7e2dcc08c71ed28814a07c18ff9308340a31
Keith Stanley <46853837+iamstan13y@users.noreply.github.com>
2022-02-15 16:52:48 +0200

    configs for production.

--- PocketBook/Startup.cs
+++ PocketBook/Startup.cs
@@ -44,7 +44,7 @@ namespace PocketBook
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
